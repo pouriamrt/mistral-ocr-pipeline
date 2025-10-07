56f68028866d5ff257c01cd46cd259c8f79a340b
Pouria Mortezaagha <pouriamortezaagha7@gmail.com>
2025-10-07 19:29:53 -0400

    made image inclusion optional

--- get_annotations.py
+++ get_annotations.py
@@ -4,18 +4,30 @@ from extration_payload import Image, ExtractionPayload
 from mistralai.models import OCRResponse
 
 
-def get_annotations(client: Mistral, base64_pdf: str) -> OCRResponse:
+def get_annotation(client: Mistral, base64_pdf: str, image_annotation: bool = False) -> OCRResponse:
     # OCR Call with Annotations
-    annotations_response = client.ocr.process(
-        model="mistral-ocr-latest",
-        pages=list(range(8)), 
-        document={
-            "type": "document_url",
-            "document_url": f"data:application/pdf;base64,{base64_pdf}"
-        },
-        bbox_annotation_format=response_format_from_pydantic_model(Image),
-        document_annotation_format=response_format_from_pydantic_model(ExtractionPayload),
-        include_image_base64=True
-    )
+    if image_annotation == True:
+        annotations_response = client.ocr.process(
+            model="mistral-ocr-latest",
+            pages=list(range(8)), 
+            document={
+                "type": "document_url",
+                "document_url": f"data:application/pdf;base64,{base64_pdf}"
+            },
+            bbox_annotation_format=response_format_from_pydantic_model(Image),
+            document_annotation_format=response_format_from_pydantic_model(ExtractionPayload),
+            include_image_base64=True
+        )
+    else:
+        annotations_response = client.ocr.process(
+            model="mistral-ocr-latest",
+            pages=list(range(8)), 
+            document={
+                "type": "document_url",
+                "document_url": f"data:application/pdf;base64,{base64_pdf}"
+            },
+            document_annotation_format=response_format_from_pydantic_model(ExtractionPayload),
+            include_image_base64=False
+        )
 
     return annotations_response
--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from mistralai import Mistral
 from dotenv import load_dotenv
 import os
 from to_markdown import convert_to_markdown
-from get_annotations import get_annotations
+from get_annotations import get_annotation
 from utils import encode_pdf
 from pathlib import Path
 from tqdm import tqdm
+from time import time
 import json
 
 load_dotenv(override=True)
@@ -14,13 +15,18 @@ def main():
     api_key = os.getenv("MISTRAL_API_KEY")
     client = Mistral(api_key=api_key)
 
-    for pdf_path in tqdm(Path("papers").glob("*.pdf")):
+    list_of_pdfs = list(Path("papers").glob("*.pdf"))
+    start_time = time()
+    for pdf_path in tqdm(list_of_pdfs, desc="Processing PDFs", total=len(list_of_pdfs)):
         base64_pdf = encode_pdf(pdf_path=pdf_path)
-        annotations_response = get_annotations(client, base64_pdf)
-        # response_dict = json.loads(annotations_response.model_dump_json())
-        # print(response_dict)
+        annotations_response = get_annotation(client, base64_pdf)
+        response_dict = json.loads(annotations_response.model_dump_json())
+        print(json.dumps(response_dict, indent=4))
         convert_to_markdown(annotations_response, f"output/{pdf_path.stem}.md")
         break
-
+    
+    end_time = time()
+    print(f"Time taken: {end_time - start_time} seconds")
+    
 if __name__ == "__main__":
     main()
